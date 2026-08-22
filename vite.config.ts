2679a4c95adfdd60e9782c4434f501b875b55124
matvey <tsentrovskiy10@mail.ru>
2026-08-22 16:34:35 +0300

    Publish Dark Day Detailing

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import { sites } from '@openai/sites-vite-plugin'
-
-export default defineConfig({
-  base: './',
-  plugins: [react(), sites()],
-  test: { environment: 'jsdom', setupFiles: './src/test/setup.ts', css: true, globals: true, include: ['src/**/*.test.tsx'] },
-})
