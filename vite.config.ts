651cbbab15a4832d822cae1d5cd005681a094ff5
matvey <tsentrovskiy10@mail.ru>
2026-08-22 13:56:24 +0300

    Publish ZENIT GARAGE on permanent Pages URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { sites } from '@openai/sites-vite-plugin'
 
 export default defineConfig({
+  base: './',
   plugins: [react(), sites()],
   test: { environment: 'jsdom', setupFiles: './src/test/setup.ts', css: true, globals: true, include: ['src/**/*.test.tsx'] },
 })
